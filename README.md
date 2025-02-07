8ff7282c1d9aa4fb305055d1fe1c91ecbe385836
Ignacio Vázquez <vazquez.ignacio@gmail.com>
2025-02-07 13:13:27 -0300

    Uploading remote changes

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 <p>El README es esencial para comunicar cómo utilizar y entender un proyecto. Este archivo es el punto de referencia inicial para cualquier persona interesada en entender tu proyecto. Un buen README incluye información sobre la instalación, dependencias, cómo ejecutar el proyecto, y posibles problemas o soluciones.
 </p>
 <p>Como reto adicional, crea un README para tu proyecto de "Amigo Secreto" detallando sus funcionalidades, incluyendo capturas de pantalla o videos que muestren cómo agregar nombres y realizar el sorteo. Esta práctica mejorará la documentación, y hará que tu proyecto sea más accesible y profesional para la comunidad.</p>
+
+<p align="left"><img src="https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green"></p>
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Comentario de Alura: El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+##################################
+Creado por: Alura
+Modificado por: Ignacio Vázquez
+Ultima modificación: 2025-02-07
+Contenido: Este archivo contiene 
+todo el código de JavaScript 
+que se utilizará en la página web 
+para el Challenge: Amigo Secreto.
+##################################
+
+2025-02-07_IV: El formato de mis comentarios será utilizando la fecha, guión bajo y mis iniciales.
+*/
+
+
+//2025-02-07_IV: Definición de Variables
+
+
+
+
+
+
+//2025-02-07_IV: Definición de Funciones
+function agregarAmigo() {
+
+}
+
+function sortearAmigo() {
+
+}
+
+
+//2025-02-07_IV: EOF (Fin de Archivo)
