89c5a98132e70c2b7de45f80b9323d46c3923bcd
Ignacio Vázquez <vazquez.ignacio@gmail.com>
2025-03-10 18:38:08 -0300

    reorden

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-//Comentario de Alura: El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 /*
-##################################
+########################################################################################################################################
 Creado por: Alura
 Modificado por: Ignacio Vázquez
 Ultima modificación: 2025-02-07
@@ -8,16 +7,18 @@ Contenido: Este archivo contiene
 todo el código de JavaScript 
 que se utilizará en la página web 
 para el Challenge: Amigo Secreto.
-##################################
 
-2025-02-07_IV: El formato de mis comentarios será utilizando la fecha, guión bajo y mis iniciales.
+Comentario de Alura: El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+Aquí deberás desarrollar la lógica para resolver el problema.
+
+########################################################################################################################################
 */
 
-//2025-02-07_IV: Definición de Variables
+// 2025-02-07_IV: El formato de mis comentarios será utilizando la fecha, guión bajo y mis iniciales.
+// 2025-02-07_IV: Definición de Variables
 
 
-
-//2025-02-07_IV: Definición de Funciones
+// 2025-02-07_IV: Definición de Funciones
 function agregarAmigo() {
 
 }
@@ -27,4 +28,4 @@ function sortearAmigo() {
 }
 
 
-//2025-02-07_IV: EOF (Fin de Archivo)
+// 2025-02-07_IV: EOF (Fin de Archivo)
